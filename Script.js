7096e732470c1c56dfb9c887cee4c6ea65d75543
Ryan Short <Short.ryand@gmail.com>
2022-08-08 08:45:32 -0700

    Update Script.js
    -Add bot object with name and some sample responses.
    -Added randomResponse() function to test.

--- Script.js
+++ Script.js
@@ -1,1 +1,12 @@
-console.log("Dude, like you totally need more friends.");
+let bot = {
+    firstName: 'Chatbot',
+    lastName: '2.0',
+    introResponses: ['Dude, like, ', 'Bro, ', ''],
+    middleResponses: ['that was seriously gnarly '],
+    endingResponses: ['bro.'],
+    randomResponse() {
+        return this.introResponses[0]+this.middleResponses[0]+this.endingResponses[0];
+    }
+}
+
+console.log(bot.randomResponse());
