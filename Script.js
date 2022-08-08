920d4505b0b3ba763f80d792282e0220550793c5
Ryan Short <Short.ryand@gmail.com>
2022-08-08 08:55:06 -0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,11 +1,14 @@
 let bot = {
     firstName: 'Chatbot',
     lastName: '2.0',
-    introResponses: ['Dude, like, ', 'Bro, ', ''],
-    middleResponses: ['that was seriously gnarly '],
-    endingResponses: ['bro.'],
+    introResponses: ['Dude, like, ', 'Bro, ', 'Sick, '],
+    middleResponses: ['that was seriously gnarly ', 'that was out of this world ', 'I am so stoked for you '],
+    endingResponses: ['bro.', 'my dude.'],
     randomResponse() {
-        return this.introResponses[0]+this.middleResponses[0]+this.endingResponses[0];
+        let randInt1 = Math.floor(Math.random() * 3);
+        let randInt2 = Math.floor(Math.random() * 3);
+        
+        return this.introResponses[randInt1]+this.middleResponses[randInt2]+this.endingResponses[0];
     }
 }
 
